85aaea845b155db0972803453369fd81d8275f88
Bernardo Mendonça <bernardomen@gmail.com>
2018-12-01 00:45:07 +0000

    added otakeys plugin

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-plugin-test-otakeys.otakeys",
+    "file": "plugins/cordova-plugin-test-otakeys/www/otakeys.js",
+    "pluginId": "cordova-plugin-test-otakeys",
+    "clobbers": [
+      "otakeys"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-test-otakeys": "0.0.1"
 };
 // BOTTOM OF METADATA
 });
